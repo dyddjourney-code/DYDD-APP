2426dc2c3dfd6d67f1595e969f42abea725e278a
John Willoughby <dyddjourney@gmail.com>
2026-09-16 21:11:53 +0000

    Simplify FruitLife support footer

--- components/fruitlife-mini-footer.tsx
+++ components/fruitlife-mini-footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function FruitLifeMiniFooter() {
   return (
     <footer className="fruitlife-mini-footer" aria-label="FruitLife support links">
-      <span>Support: support@discoverdivine.design</span>
+      <a href="mailto:support@discoverdivine.design">Support</a>
       <Link href="/privacy">Privacy Policy</Link>
       <Link href="/terms">Terms of Service</Link>
     </footer>
